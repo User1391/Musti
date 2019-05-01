2e411116f01a0be03a3e767a6e3462f635ac6921
User1391 <max.penders@westtown.edu>
2019-05-01 13:37:11 -0400

    I am sexy

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ pygame.display.set_caption("Space Create")
 # Sprites
 images = [pygame.image.load('imgs/Rocket.png'), pygame.image.load('imgs/Asteroid.png')]
 ## rocket ##
-rocket = Body(win, images[0], "Rocket", ROCKET_MASS)
-rocket.scaleImg((200,200))
+rocket = Body(win, images[0], (200, 200), ROCKET_MASS)
+
 
 #######
 # asteroid
-asteroid = pygame.transform.scale(images[1], (200,200))
+asteroid = Body(win, images[1], (200,200), ASTEROID_MASS)
 
 
 # start the clock
@@ -41,7 +41,7 @@ while running:
     clock.tick(FPS)
     # fill in background color
     rocket.mov((ROCKETX, ROCKETY))
-    
+
 
     #movement
     ROCKETX += SPEED
--- physics.py
+++ physics.py
@@ -2,32 +2,33 @@ import pygame
 ## PHYSICS ##
 class Body(object):
     pygame = __import__('pygame')
-    def __init__(self, frameName, img, name, mass=0):
-        self.nam = name
+    
+    def __init__(self, frameName, img, size, mass=0):
+        self.size = size
         self.mas = mass
         self.frameName = frameName
-        self.image = img
+        self.image = pygame.transform.scale(img, self.size)
 
-    def name(self):
-        return self.nam
+    def getSize(self):
+        return self.size
 
-    def mass(self):
+    def getMass(self):
         return self.mas
 
-    def pos(self):
+    def getPos(self):
         return self.posi
 
-    def img(self):
+    def getImg(self):
         return self.image
 
-    def framename(self):
+    def getframename(self):
         return self.frameName
 
     def mov(self, newPos):
         self.frameName.blit(self.image , newPos)
         return
 
-    def scaleImg(self, size):
+    def scale(self, size):
         self.image = pygame.transform.scale(self.image, size)
         return
 
